dd8a3b342075662a8dfc96aa2c0606ea98668834
Luiz Kim <luiz.kim@controleonline.com>
2020-09-18 17:25:21 -0300

    Add composer files

--- src/service/freteclick.php
+++ src/service/freteclick.php
@@ -1,29 +1,42 @@
 <?php
 namespace SDK\Service;
 
-use guzzlehttp\guzzle AS API;
 use SDK\Models\origin;
 use SDK\Models\destination;
 use SDK\Models\order;
 use SDK\Models\quote;
 
 class freteclick{
+
+	private $url = 'https://api.freteclick.com.br';
 	private $api_key = NULL;	
+	private $api = NULL;	
 	
 	private function __construct(){}
 
 	protected static function getInstance($api_key){
 		$this->api_key = $api_key;
+		$this->api = new \GuzzleHttp\Client(
+			[
+				'url' => $this->url,
+				'headers' => [ 
+					'Accept' => 'application/json',
+            		'content-type' => 'application/ld+json',
+					'api-token' => $this->api_key
+				]
+			]);
 		return $this;
 	}
 
-	public function quote(origin $origin,destination $destination){
-
-		$api = new API();
-		/*
-		* Implementar a consulta
-		*/		
-
+	public function quote(origin $origin,destination $destination){		
+		$request = $this->api->get(
+			array(
+				'origin'		=> $origin,
+				'destination'	=> $destination
+			)
+		);
+		$response = $request->send();
+		return $response->getBody();
 	}
 
 }
